a4e15625121712c39a1f60bd0433efa346db42b1
PabloDev09 <pmg11@gmail.com>
2025-03-10 12:37:04 +0100

    AFA Version 1.0.0: 10/03/2025

--- lib/logic/router/path/path_url_afa.dart
+++
@@ -1,8 +0,0 @@
-class PathUrlAfa 
-{
-  final String pathWelcome = "/";
-  final String pathLogin = "/login";
-  final String pathRegister = "/register";
-  final String pathDashboard = "/dashboard";
-  final String pathMap = "/map";
-}
